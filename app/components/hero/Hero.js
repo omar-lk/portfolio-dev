19e5d350dda6487ee8c78c0fb18c49684db2133b
omar-lk <omar.lekouche.ext@jpme.fr>
2026-03-05 00:08:07 +0000

    hero build fix

--- app/components/hero/Hero.js
+++ app/components/hero/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
         <div className={styles.aboutContainer}>
           <h1>hey, I&apos;m Omar👋.</h1>
           <h2>
-            I&apos;m a Senior Full Stack Engineer with 8+ years of experience
+            I&apos;m a Senior Full Stack Engineer with 9+ years of experience
             building production web platforms for European companies. Most
             recent work: a complex internal platform for a French energy company
             — contract management, billing automation, invoice generation, and
